0c05481ac3fdc74dd9094df2ac629a81b9bcf54b
Jaskirat <zjaskiratsingh@gmail.com>
2025-02-01 23:58:57 +0530

    insert operation completed

---
+++ boot.php
@@ -0,0 +1,142 @@
+<?php
+$servername = "localhost";
+$usrename = "root";
+$password = "";
+$database = "notes";
+
+$conn = mysqli_connect($servername ,$usrename , $password, $database);
+if(!$conn){
+  die("Failed to connect: " . mysqli_connect_error());
+}
+else{
+  echo "Connected successfuly";
+}
+
+//Check the request
+echo $_SERVER['REQUEST_METHOD'];
+
+//Insert data into database
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $title = $_POST['title'];
+  $disc = $_POST['description'];
+
+  $sql = "INSERT INTO `note` (`title`, `description`, `tstamp`) VALUES ('$title', '$disc', current_timestamp())";
+  $result = mysqli_query($conn , $sql);
+  if($result){
+    <div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+  }
+  else{
+    echo "Failed";
+  }
+}
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Death Note</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <div class="container my-4">
+    <form action="/CRUD/boot.php" method="POST">
+      <div class="mb-3">
+        <label for="title" class="form-label">Note Title</label>
+        <input type="text" name="title" class="form-control" id="title">
+      </div>
+      <div class="form-group">
+        <label for="desc">Note Description</label>
+        <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+      </div>
+
+      <button type="submit" class="btn btn-primary">Add Note</button>
+    </form>
+
+
+    <div class="table_Data">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">S.No</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        
+        <?php
+        $sno = 0;
+  $sql = "SELECT * FROM `note`";
+  $result = mysqli_query($conn , $sql);
+  while($row = mysqli_fetch_assoc($result)){
+    $sno =  $sno+1;
+    echo "<tr>
+      <th scope='row'>" . $sno ."</th>
+      <td>" . $row['title'] . "</td>
+      <td>". $row['description'] ."</td>
+      <td>Action</td>
+    </tr>";
+  }
+  ?>
+
+        <tbody>
+
+        </tbody>
+      </table>
+    </div>
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
